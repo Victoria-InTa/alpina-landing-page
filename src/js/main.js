a068223469413510d44ed7eff900a0cda507c847
unknown <joelangeltw@gmail.com>
2022-01-22 15:37:20 -0800

    header

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
 import '../css/main.css'
+
+
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu')
+const toggleLine = document.querySelector('.toggle__line')
+
+toggle.addEventListener('click',()=>{
+    toggle.classList.toggle('open')
+    menu.classList.toggle('open')
+})
